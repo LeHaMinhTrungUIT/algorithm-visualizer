cf07d230088ee5a2cdef6c78b2a8b615d43e947c
LeHaMinhTrungUIT <18520390@gm.uit.edu.vn>
2021-12-14 08:14:09 +0700

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import styled from "styled-components";
+import { NavBar } from "./components/NavBar";
+import { Controller } from "./components/Controller";
+import { AlgoDisplay } from "./components/AlgoDisplay";
+import { Footer } from "./components/Footer"
+
+const Container = styled.div`
+  margin: 0 10px;
+  min-height: calc(100vh - 50px);
+  position: relative;
+  margin-bottom: 50px;
+`;
+
+export default function App() {
+  return (
+    <Container>
+      <NavBar />
+      <Controller />
+      <AlgoDisplay />
+      
+    </Container>
+  );
+}
